532595c91d57f5b7f596daa50973d8b6eb47ac90
TeooL <144486916+TeooL@users.noreply.github.com>
2024-03-07 15:28:25 -0500

    Add files via upload

--- Flute.java
+++ Flute.java
@@ -1,6 +1,6 @@
 public class Flute extends Instrument{
-    public Flute(int p){
-        super(p);
+    public Flute(int p, String name){
+        super(p,name);
     }
     public void perform(int time){
         System.out.println("It's time to perform");
--- Instrument.java
+++ Instrument.java
@@ -1,12 +1,17 @@
 public class Instrument {
+    private String name;
     private int price;
-    public Instrument(int p){
+    public Instrument(int p, String name){
         price = p;
+        this.name = name;
     }
     public void play(int time){
         System.out.println("Playing for " + time + " hours");
     }
     public void makeNoise(){
         System.out.println("Making noise!");
+    }
+    public String toString(){
+        return name;
     }
 }
