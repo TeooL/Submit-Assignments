640e7dec843ec984b4fb515c5551b78216dc5ca2
TeooL <144486916+TeooL@users.noreply.github.com>
2024-03-11 15:20:12 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[]args){
         Instrument triangle = new Instrument(25);
+        Instrument horn =  new Instrument(20);
         System.out.println(triangle);
         System.out.println(triangle.equals(new Instrument(25)));
-        Instrument grand = new Piano(200);
-        grand.play();
+        System.out.println(horn);
+        System.out.println(horn.equals(new Instrument(20)));
     }
 }
