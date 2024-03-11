e0cc29f7c16fac31dded5046c362118695c0baca
TeooL <144486916+TeooL@users.noreply.github.com>
2024-03-11 15:12:39 -0400

    Delete Flute.java

--- Flute.java
+++
@@ -1,9 +0,0 @@
-public class Flute extends Instrument{
-    public Flute(int p, String name){
-        super(p,name);
-    }
-    public void perform(int time){
-        System.out.println("It's time to perform");
-        super.play(time);
-    }
-}
