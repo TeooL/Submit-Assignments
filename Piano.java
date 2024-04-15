cb9e1b874c3f4f4c02286955cd8fd0a2fb04afd7
TeooL <144486916+TeooL@users.noreply.github.com>
2024-04-15 15:29:54 -0400

    Delete Piano.java

--- Piano.java
+++
@@ -1,9 +0,0 @@
-public class Piano extends Instrument {
-    private int numOfKeys;
-    public Piano (int p){
-        super(p);
-    }
-    public void play(){
-        System.out.println("PLAYING MOONLIGHT SONATA ~~~~~");
-    }
-}
